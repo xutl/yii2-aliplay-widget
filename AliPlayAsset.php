e1b336b1ef68976df3d83369a87c371881c92c5f
xuchao <85825770@qq.com>
2017-03-30 18:10:22 +0800

    更新播放器版本到1.6.3

--- AliPlayAsset.php
+++ AliPlayAsset.php
@@ -9,10 +9,10 @@ use yii\web\View;
 class AliPlayAsset extends \yii\web\AssetBundle
 {
     public $css = [
-        '//g.alicdn.com/de/prismplayer/1.5.7/skins/default/index-min.css',
+        '//g.alicdn.com/de/prismplayer/1.6.3/skins/default/index-min.css',
     ];
 
     public $js = [
-        '//g.alicdn.com/de/prismplayer/1.5.7/prism-min.js',
+        '//g.alicdn.com/de/prismplayer/1.6.3/prism-min.js',
     ];
 }
